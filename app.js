480e427cbf3f771bb522f8d5dbd333dc26574a5b
salih <slh32064@gmail.com>
2020-08-30 15:22:32 +0530

    Do

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ const request = require('request')
 const urls = require('./urls/url')
 
 
-const url = urls()
+const wUrl = urls.weatherStackUrl()
 
 
-request({url:url,json:true},(error,response)=>{
-    debugger
-   const data = response.body.current
-   console.log(data.weather_descriptions+'. It is currently  ' +data.temperature + ' degrees out. it is feel like  ' +data.feelslike+ '  degrees out');
+request({url:wUrl,json:true},(error,response)=>{
+    if(error){
+      console.log('Unable to connect to server');
+    }else if(response.body.error){
+      console.log('Please provide a valid location');
+    }else{
+      const data = response.body.current
+      console.log(data.weather_descriptions+'. It is currently  ' +data.temperature + ' degrees out. it is feel like  ' +data.feelslike+ '  degrees out');
+    }
+  
 })
+
+const mUrl = urls.mapBoxUrl()
+request({url:mUrl,json:true},(error,response)=>{
+    if(error){
+          console.log('can\'t connect to server ');
+    }else if(response.body.features.length === 0){
+          console.log('oops! No location found');
+    }else{
+        const longitude = response.body.features[0].center[0]
+        const latitude = response.body.features[0].center[1]
+         console.log('latitude: '+latitude  +'  longitude:  ' + longitude)
+    }
+    
+}
+)
+
