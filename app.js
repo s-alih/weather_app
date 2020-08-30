73a2c14416152096bc00086f50636bfa3ab1570e
salih <slh32064@gmail.com>
2020-08-30 13:52:44 +0530

    rw

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require('request')
+const urls = require('./urls/url')
 
-const url = 'http://api.weatherstack.com/current?access_key=b5c9f47b39a65e7a0f2a33c67c7fab32&query=10.876440,76.304207&units=f'
+
+const url = urls()
+
 
 request({url:url,json:true},(error,response)=>{
+    debugger
    const data = response.body.current
    console.log(data.weather_descriptions+'. It is currently  ' +data.temperature + ' degrees out. it is feel like  ' +data.feelslike+ '  degrees out');
 })
